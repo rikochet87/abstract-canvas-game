da09bc921275551c1e9fdc401003bf70718af139
matias <matias3282@hotmail.com>
2022-07-28 14:38:57 -0300

    create player

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import { Player } from "./player";
+
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, "blue");
+player.draw()
+
+export{canvas, c}
