3216a5dfc4724da3f59fbec3cafa0599af04462f
matias <matias3282@hotmail.com>
2022-07-30 11:45:39 -0300

    create enemies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Player } from "./player";
 import { Projectile } from "./projectiles";
+import { Enemy } from "./enemy";
 
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
@@ -13,13 +14,49 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, "blue");
 
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4;
+    let x
+    let y
+
+    if (Math.random() < 0.5 ) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height
+    }else{
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+     
+   
+    
+    const color = "green";
+    const angle = Math.atan2(
+      canvas.height / 2 - y,
+      canvas.width / 2 - x
+    );
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+    console.log(enemies);
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
+
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
@@ -38,5 +75,6 @@ addEventListener("click", (event) => {
 });
 
 animate();
+spawnEnemies();
 
 export { canvas, c };
