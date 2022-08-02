d7947c29346681fb7cf3834c85afd0f76dfff587
matias <matias3282@hotmail.com>
2022-08-02 10:19:44 -0300

    restart game

--- src/index.js
+++ src/index.js
@@ -10,16 +10,27 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 const scoreEl = document.querySelector("#scoreEl");
-const startGameBtn = document.querySelector("#startGameBtn")
-const modalEl = document.querySelector("#modalEl")
+const startGameBtn = document.querySelector("#startGameBtn");
+const modalEl = document.querySelector("#modalEl");
+const bigScoreEL = document.querySelector("#bigScoreEl");
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 10, "white");
-const projectiles = [];
-const enemies = [];
-const particles = [];
+let player = new Player(x, y, 10, "white");
+let projectiles = [];
+let enemies = [];
+let particles = [];
+
+function init() {
+  player = new Player(x, y, 10, "white");
+  projectiles = [];
+  enemies = [];
+  particles = [];
+  score = 0;
+  scoreEl.innerHTML = score;
+  bigScoreEL.innerHTML = score;
+}
 
 let animationId;
 let score = 0;
@@ -30,13 +41,13 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.draw();
 
-  particles.forEach((particle, index) =>{
-    if(particle.alpha <= 0){
-      particles.splice(index, 1)
-    }else{
-      particle.update()
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
     }
-  })
+  });
 
   projectiles.forEach((projectile, index) => {
     projectile.update();
@@ -62,6 +73,8 @@ function animate() {
     //end game
     if (dist - enemy.radius - player.radius < 1) {
       cancelAnimationFrame(animationId);
+      modalEl.style.display = "flex";
+      bigScoreEL.innerHTML = score;
     }
 
     projectiles.forEach((projectile, projectileIndex) => {
@@ -69,22 +82,26 @@ function animate() {
 
       // when projectiles touch enemy
       if (dist - enemy.radius - projectile.radius < 1) {
-
         //create explosions
         for (let i = 0; i < enemy.radius * 2; i++) {
           particles.push(
-            new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
-              x: (Math.random() - 0.5)*(Math.random()*6),
-              y: (Math.random() - 0.5)*(Math.random()*6)
-            })
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 6),
+                y: (Math.random() - 0.5) * (Math.random() * 6),
+              }
+            )
           );
         }
 
         if (enemy.radius - 10 > 5) {
-
-        //increase our score
-        score += 100
-        scoreEl.innerHTML = score
+          //increase our score
+          score += 100;
+          scoreEl.innerHTML = score;
 
           gsap.to(enemy, {
             radius: enemy.radius - 10,
@@ -94,8 +111,8 @@ function animate() {
           }, 0);
         } else {
           //remove from scene altogether
-        score += 250
-        scoreEl.innerHTML = score
+          score += 250;
+          scoreEl.innerHTML = score;
           setTimeout(() => {
             enemies.splice(index, 1);
             projectiles.splice(projectileIndex, 1);
@@ -120,10 +137,11 @@ addEventListener("click", (event) => {
   );
 });
 
-startGameBtn.addEventListener("click", () =>{
+startGameBtn.addEventListener("click", () => {
+  init()
   animate();
   spawnEnemies();
-  modalEl.style.display = "none"
-})
+  modalEl.style.display = "none";
+});
 
 export { canvas, c, enemies };
